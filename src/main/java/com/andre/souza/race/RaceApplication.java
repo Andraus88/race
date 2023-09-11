2c703713cf1fb1b9e0b2ad0bd55555bfbe33859a
Rausch <drerausch@gmail.com>
2023-09-11 16:35:24 -0300

    Crud initial VERSION THREE on MySQL please don't break

--- src/main/java/com/andre/souza/race/RaceApplication.java
+++ src/main/java/com/andre/souza/race/RaceApplication.java
@@ -1,10 +1,17 @@
 package com.andre.souza.race;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RaceApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaceApplication.class, args);
